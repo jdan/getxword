f13a32c38234901f18471e5639c2083a934ad40b
Jordan Scales <scalesjordan@gmail.com>
2023-06-11 11:32:16 -0400

    let's roll

---
+++ fetch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# current date i.e. Jun1123
+# include the last 2 digits of the year
+DATE=$(date +"%b%d%y")
+
+# read cookie from cookie.txt
+COOKIE=$(cat cookie.txt)
+
+curl "https://www.nytimes.com/svc/crosswords/v2/puzzle/print/$DATE.pdf" \
+  --silent \
+  -o $DATE.pdf \
+  -H 'authority: www.nytimes.com' \
+  -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'accept-language: en-US,en;q=0.9' \
+  -H 'cache-control: max-age=0' \
+  -H "cookie: ${COOKIE}" \
+  -H 'dnt: 1' \
+  -H 'referer: https://www.nytimes.com/crosswords' \
+  -H 'sec-ch-ua: "Not.A/Brand";v="8", "Chromium";v="114"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "macOS"' \
+  -H 'sec-fetch-dest: document' \
+  -H 'sec-fetch-mode: navigate' \
+  -H 'sec-fetch-site: same-origin' \
+  -H 'sec-fetch-user: ?1' \
+  -H 'upgrade-insecure-requests: 1' \
+  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36' \
+  --compressed
